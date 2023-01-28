18fcf21308538f35afaf43fe1baa6bcee84ffd0f
callumjfortune <callum.fortune@bbc.co.uk>
2023-01-28 04:02:30 +0000

    new start

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-var openai = config.OPEN_AI;
+var apiKeyStart = "sk-rF1Wm9crTFFLEXXBzUgcT3B"
+var apiKeyEnd = "lbkFJ6oQ1Qiqg7ioGzeIDkGk1"
+
+function makeAjaxCall() {
+    const apiKey = apiKeyStart + apiKeyEnd;
+    const endpoint = 'https://api.openai.com/v1/engines/davinci/completions';
+  
+    const prompt = 'What is the capital of France?';
+  
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+        temperature: 0.7
+      })
+    };
+  
+    fetch(endpoint, options)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  
+  
+  
+  
+  
